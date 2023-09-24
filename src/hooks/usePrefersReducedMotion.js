03e52745c094c83f76708d91094777541f826c03
cubinCheese <linalbert24@gmail.com>
2023-09-24 04:28:56 -0700

    modified usePrefersReducedMotion.js, replacing deprecated eventListener functions

--- src/hooks/usePrefersReducedMotion.js
+++ src/hooks/usePrefersReducedMotion.js
@@ -3,27 +3,31 @@
  */
 
 import { useState, useEffect } from 'react';
+
 const QUERY = '(prefers-reduced-motion: no-preference)';
 const isRenderingOnServer = typeof window === 'undefined';
 
-const getInitialState = () =>
-  // For our initial server render, we won't know if the user
-  // prefers reduced motion, but it doesn't matter. This value
-  // will be overwritten on the client, before any animations
-  // occur.
-  isRenderingOnServer ? true : !window.matchMedia(QUERY).matches;
+const getInitialState = () => (isRenderingOnServer ? true : !window.matchMedia(QUERY).matches);
+
 function usePrefersReducedMotion() {
   const [prefersReducedMotion, setPrefersReducedMotion] = useState(getInitialState);
+
   useEffect(() => {
     const mediaQueryList = window.matchMedia(QUERY);
+
     const listener = event => {
       setPrefersReducedMotion(!event.matches);
     };
-    mediaQueryList.addListener(listener);
+
+    // Use addEventListener instead of addListener
+    mediaQueryList.addEventListener('change', listener);
+
     return () => {
-      mediaQueryList.removeListener(listener);
+      // Use removeEventListener instead of removeListener
+      mediaQueryList.removeEventListener('change', listener);
     };
   }, []);
+
   return prefersReducedMotion;
 }
 
